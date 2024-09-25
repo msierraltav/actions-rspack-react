3170bbfe106fd7e0a6a21a53e856cc7a9d21eadd
Mitch <115440917+msierraltav@users.noreply.github.com>
2024-09-24 20:33:21 -0600

    first commit, interview question as example

---
+++ README.md
@@ -0,0 +1,28 @@
+# Github action playground, rspack, axios and chocolate starfish with hot dog flavored water.
+
+A github action example and by chance a test to try RSpack with react and test some interactions using axios.
+
+
+## Setup
+
+optional : install pnpm.
+
+Install the dependencies:
+
+```bash
+pnpm install
+```
+
+## Get Started
+
+Start the dev server:
+
+```bash
+pnpm run dev
+```
+
+Build the app for production:
+
+```bash
+pnpm run build
+```
---
+++ src/App.tsx
@@ -0,0 +1,89 @@
+import React from "react";
+import { useState } from "react";
+import "./App.css";
+import { useEffect } from "react";
+// method one, is possible to get the data from the json file
+// simply importing it.
+// import dataJsonFile from "./assets/data.json"
+
+interface TData {
+  id: number;
+  name: string;
+}
+
+function App() {
+  // this was a interview question, take data from the data.json
+  // then show it in a list
+  // and ina search component filter the list
+  // when a element of the list is clicked , delete it.
+
+  const [initialData, setInitialData] = useState<TData[]>([]);
+  const [dataList, setDataList] = useState<TData[]>([]);
+
+  const handleDelete = (id) => {
+    setDataList(dataList.filter((item) => item.id !== id));
+  }
+
+  const handleSearchText = (text) => {
+    if(text.length > 2){
+      setDataList(dataList.filter((item) => item.name.toLowerCase().includes(text.toLowerCase())));
+    }else{
+      setDataList(initialData);
+    }
+  }
+
+  // using axios. the tip is the response from the file is "data"
+  const axios = require("axios");
+  useEffect(() => {
+    const getData = async () => {
+      await axios.get("/data.json").then((response) => {
+        return response.data;
+      })
+      .then((data) => {
+        setInitialData(data);
+        setDataList(data);
+      })
+      .catch(error => console.log(error));
+    };
+
+    getData();
+  }, []);
+
+  // using fetch
+  // really it was too easy??
+  // useEffect(() => {
+  //   const getData = async () => {
+  //     await fetch("/data.json")
+  //     .then((response) => response.json())
+  //     .then((data) => {
+  //       setDataList(data);
+  //     })
+  //     .catch(error => console.log(error));
+  //   };
+  //   getData();
+  // },[])
+
+  return (
+    <>
+      <h1>List searcher!!! </h1>
+      <div>
+        <input type="text"
+          placeholder="search..."
+          onChange={(e) => {handleSearchText(e.target.value)}}
+        >
+        </input>
+      </div>
+      <ul>
+        {dataList.length > 0  && dataList.map((data) => (
+            <li key={data.id}
+              onClick={() => {handleDelete(data.id)}}
+            >
+              {data.name}
+            </li>
+          ))}
+      </ul>
+    </>
+  );
+}
+
+export default App;
